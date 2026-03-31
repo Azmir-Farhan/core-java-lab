604fb731a2db007b867be8f776addf388a8d6051
Azmir-Farhan <azmirfarhan2026@gmail.com>
2026-03-31 16:58:44 +0500

    Add files via upload

---
+++ IntegerToBinary.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+class Binary
+{
+    public static void main(String[] args) 
+    {
+        Scanner input = new Scanner(System.in);
+        
+        // Assignment_A
+        int num;
+        System.out.println("Enter a Integer Number:");
+        num = input.nextInt();
+        
+        if (num == 0) 
+            {
+                System.out.println(0);
+            }
+        
+        int arr[] = new int [32]; //Store Bits
+        int i = 0;
+
+        while (num > 0) 
+            {
+                arr[i] = num % 2;
+                num = num / 2;
+                i++;
+            }
+        // Printing Message
+        for (int j = i - 1; j >= 0; j--)    
+        {
+            System.out.print(arr[j]);
+        }
+        input.close
+    }
+}
